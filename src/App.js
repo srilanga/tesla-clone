04d100cf3a6beaff7adba7bbc1d227bc1335389a
srilanga <2983rgt@gmail.com>
2022-05-06 13:38:19 +0900

    ADD: redux for car

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from "./components/Header" // Added to use Header.js
 import Home from "./components/Home" // Added to use Home tag from Home.js
